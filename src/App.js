708d18ea06215d8c5e1d5dd8a31a52d4fc13b2ca
Rokas <rookas.rudzenskas@gmail.com>
2021-06-22 12:56:30 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -9,3 +9,10 @@ function App() {
 }
 
 export default App;
+
+
+// secret 👉
+// EKZTghP7F256UeFPvdhUm2_9Xb4FqP1SsyQZA_7LFjYFU1Amc-k-I5E0a9FZoXdLHgBkcdOsJdmkTR8u
+
+// client id 👉
+// ATg8GTv4VioCaiiXrPxvpn40-nAVMwkj6o3bZxzFq65ldVMkwxP-1PVwdijsFzZpMghH5KYiIUx1Cs3-
